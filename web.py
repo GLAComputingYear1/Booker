3dce82ab65fbf6ed21039a49e155eeff9c0c8e02
lukeo102 <lukeormiston@gmail.com>
2022-01-10 19:24:47 +0000

    Made www folder

--- web.py
+++ web.py
@@ -3,12 +3,12 @@ from os import path
 
 class web:
     def __init__(self):
-        self.env = env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
+        self.env = env = Environment(loader=FileSystemLoader("www"), autoescape=select_autoescape())
         self.template = env.get_template("index_template.html")
 
         self.make()
     
     def make(self):
-        with open("index.html", "w") as file:
+        with open("www/index.html", "w") as file:
 
             file.write(self.template.render(test="testing", onetwothree="123"))
