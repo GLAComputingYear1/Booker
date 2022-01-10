3c91131eeb70296c489628d8428c197f37563505
lukeo102 <lukeormiston@gmail.com>
2022-01-10 19:21:28 +0000

    LOOK AT ME BEN IM DOING STUFF

--- web.py
+++ web.py
@@ -1,10 +1,14 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from os import path 
 
-env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
+class web:
+    def __init__(self):
+        self.env = env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
+        self.template = template = env.get_template("index_template.html")
 
-template = env.get_template("index_template.html")
+        self.make()
+    
+    def make(self):
+        with open("index.html", "w") as file:
 
-with open("index.html", "w") as file:
-
-    file.write(template.render(test="testing", onetwothree="123"))
+            file.write(self.template.render(test="testing", onetwothree="123"))
