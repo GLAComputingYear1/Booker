ac04d5597e7bbd5cc10da999e72f1497b2d558e8
lukeo102 <lukeormiston@gmail.com>
2022-01-11 14:32:45 +0000

    clean up

--- web.py
+++ web.py
@@ -6,19 +6,15 @@ class web:
     def __init__(self):
         self.env = env = Environment(loader=FileSystemLoader("www"), autoescape=select_autoescape())
         self.template = env.get_template("index_template.html")
-
         self.make()
     
     def format_day(self, date):
         
         datetime_date = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-        
         worded_day = datetime_date.strftime('%A')
         numbered_day = datetime_date.strftime('%Y-%m-%d')
-        
-        full_day = f'{worded_day} {numbered_day}'
-        
-        return full_day
+                
+        return f'{worded_day} {numbered_day}'
     
     def make(self):
         
@@ -34,12 +30,8 @@ class web:
             day_string = self.format_day(day[0]['session_start'])
             room_bookings.append([day_string])
             
-            for j, booking in enumerate(day):
+            for booking in day:
                 room_bookings[i].append(booking['room_name'])
-        
-        print(room_bookings)
         
         with open("www/index.html", "w") as file:
             file.write(self.template.render(room_bookings=room_bookings))
-            
-web = web()
